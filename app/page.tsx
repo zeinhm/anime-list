61dcd37f0fdfdfcc33ac2d045e736d30fd903974
Zein Hammad Marhabah <zein.hammad@sera.astra.co.id>
2024-06-03 17:33:35 +0700

    feat: add loading card skeletons

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
-import AnimeList from "@/components/molecules/AnimeList";
 import { fetchAnimes } from "@/api/fetchAnimes";
 import { AnimesResponseTypes } from "@/types/fetchAnimeParams";
-import { useState } from "react";
+
 import useDebounce from "@/hooks/useDebounce";
+
 import Input from "@/components/atoms/Input";
-import { SearchIcon } from "@/components/icons";
-import StaticJumbotron from "@/components/molecules/StaticJumbotron";
+import AnimeList from "@/components/molecules/AnimeList";
 import Pagination from "@/components/molecules/Pagination";
+import StaticJumbotron from "@/components/molecules/StaticJumbotron";
+import AnimeCardSkeleton from "@/components/skeletons/AnimeCardSkeleton";
+import { SearchIcon } from "@/components/icons";
 
 export default function Home() {
   const [page, setPage] = useState(1);
@@ -31,9 +34,6 @@ export default function Home() {
     setPage(1);
   };
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error</div>;
-
   return (
     <div className="p-16 pt-0 grid grid-cols-5">
       <div className="col-start-2 col-span-3">
@@ -48,20 +48,40 @@ export default function Home() {
             icon={<SearchIcon className="text-gray-400" />}
           />
         </form>
-        {data ? (
-          <>
-            <AnimeList animes={data.data} />
-            <Pagination
-              pageCount={Math.ceil(
-                data.pagination.items.total / data.pagination.items.per_page
+        <div className="min-h-[420px]">
+          {isLoading ? (
+            <AnimeCardSkeleton />
+          ) : (
+            <>
+              {isError ? (
+                <div className="text-gray-200">
+                  The server encountered error and could not complete your
+                  request. Please try again later
+                </div>
+              ) : (
+                <>
+                  {data && data?.data.length > 0 ? (
+                    <>
+                      <AnimeList animes={data.data} />
+                      <Pagination
+                        pageCount={Math.ceil(
+                          data.pagination.items.total /
+                            data.pagination.items.per_page
+                        )}
+                        onPageChange={(e) => handlePageClick(e)}
+                        page={page - 1}
+                      />
+                    </>
+                  ) : (
+                    <div className="pl-4 text-gray-200">
+                      No relevant results found
+                    </div>
+                  )}
+                </>
               )}
-              onPageChange={(e) => handlePageClick(e)}
-              page={page - 1}
-            />
-          </>
-        ) : (
-          <div className="pl-4 text-gray-200">No relevant results found</div>
-        )}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ components/skeletons/AnimeCardSkeleton.tsx
@@ -0,0 +1,17 @@
+export default function AnimeCardSkeleton() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div
+          key={`my-order-list-${i}-shimmer`}
+          className="flex flex-col bg-[#161616] rounded-xl shadow-lg overflow-hidden animate-shimmer"
+        >
+          <div className="flex flex-col items-center gap-6 text-sm">
+            <div className="h-[180px] w-full bg-[#212121] animate-pulse"></div>
+            <div className="h-[24px] w-4/5 mb-6 rounded-full bg-[#212121] animate-pulse"></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
